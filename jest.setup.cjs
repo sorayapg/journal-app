cb8fbbee8cb8962e1f563881859d1b2d6a2e11e0
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-04-14 19:27:38 +0200

    Refactor jest.setup.cjs to use import statement for getEnvironments and improve module organization

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -1,1 +1,13 @@
+import { getEnvironments } from("./src/helpers/getEnvironments");
+
+
 module.exports = require("whatwg-fetch");
+
+require('dotenv').config({
+    path: '.env.test',
+});
+
+jest.mock('./src/helpers/getEnvironments', () => ({
+    getEnvironments : () => ({...process.env}),
+
+}))
