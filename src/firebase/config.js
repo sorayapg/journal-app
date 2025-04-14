de53cc3904b01063f800390169941c97e9b558e0
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-04-14 19:31:41 +0200

    Update Firebase configuration to use production credentials and clean up commented code

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -16,24 +16,24 @@ console.log( env);
 // Your web app's Firebase configuration
 
 //Dev/prod
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAyeCJUGZFxtt0a8X2ObiWftc2Bm722waU",
-//   authDomain: "cursos-react-4ec56.firebaseapp.com",
-//   projectId: "cursos-react-4ec56",
-//   storageBucket: "cursos-react-4ec56.firebasestorage.app",
-//   messagingSenderId: "938134825001",
-//   appId: "1:938134825001:web:55040505d6e930c1881438"
-// };
-
-// Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyAagzUSFHCDYRA0_fpicDAQ24YJbrAnaCA",
-  authDomain: "react-cursos-40001.firebaseapp.com",
-  projectId: "react-cursos-40001",
-  storageBucket: "react-cursos-40001.firebasestorage.app",
-  messagingSenderId: "590242697599",
-  appId: "1:590242697599:web:dfc485c9abe29b5b834a4a"
+  apiKey: "AIzaSyAyeCJUGZFxtt0a8X2ObiWftc2Bm722waU",
+  authDomain: "cursos-react-4ec56.firebaseapp.com",
+  projectId: "cursos-react-4ec56",
+  storageBucket: "cursos-react-4ec56.firebasestorage.app",
+  messagingSenderId: "938134825001",
+  appId: "1:938134825001:web:55040505d6e930c1881438"
 };
+
+// // Testing
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAagzUSFHCDYRA0_fpicDAQ24YJbrAnaCA",
+//   authDomain: "react-cursos-40001.firebaseapp.com",
+//   projectId: "react-cursos-40001",
+//   storageBucket: "react-cursos-40001.firebasestorage.app",
+//   messagingSenderId: "590242697599",
+//   appId: "1:590242697599:web:dfc485c9abe29b5b834a4a"
+// };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
