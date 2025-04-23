ebf75d9a9de25f645e4d550b83c9ac2ddef225c7
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-04-23 17:51:33 +0200

    Enhance accessibility and clean up debug code in LoginPage component

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -28,7 +28,7 @@ export const LoginPage = () => {
   const onSubmit = ( event ) => {
     event.preventDefault();
 
-    // console.log({ email, password })
+    //console.log({ email, password })
     dispatch( startLoginWithEmailPassword({ email, password }) );
   }
 
@@ -41,7 +41,10 @@ export const LoginPage = () => {
   return (
     <AuthLayout title="Login">
       {/* <h1>hola mundo {import.meta.env.VITE_HOLA} </h1> */}
-      <form onSubmit={ onSubmit } className='animate__animated animate__fadeIn animate__faster'>
+      <form 
+        aria-label= "submit-form" 
+        onSubmit={ onSubmit } 
+        className='animate__animated animate__fadeIn animate__faster'>
           <Grid container>
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
               <TextField 
@@ -62,6 +65,9 @@ export const LoginPage = () => {
                 placeholder='Contraseña' 
                 fullWidth
                 name="password"
+                inputProps={{
+                  'data-testid': 'password'
+                }}
                 value={ password }
                 onChange={ onInputChange }
               />
@@ -95,6 +101,7 @@ export const LoginPage = () => {
                    disabled={ isAuthenticating }
                    variant='contained' 
                    fullWidth
+                   aria-label="google-btn"
                    onClick={ onGoogleSignIn }>
                   <Google />
                   <Typography sx={{ ml: 1 }}>Google</Typography>
